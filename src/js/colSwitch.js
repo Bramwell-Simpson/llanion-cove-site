c68da6db1ed0a619c344886adc1a7eed1453d67d
Bramwell Simpson <bramwellsimpson1@gmail.com>
2023-05-16 09:54:23 +0100

    Change column sizes

--- src/js/colSwitch.js
+++ src/js/colSwitch.js
@@ -2,12 +2,12 @@ $(window).on('resize', function() {
     if($(window).width() > 1024) {
         $('#act-title').addClass('col-5');
         $('#act-title').removeClass('col-12');
-        $('#act-desc').addClass('col-5');
+        $('#act-desc').addClass('col-7');
         $('#act-desc').removeClass('col-12');
     }else{
         $('#act-title').addClass('col-12');
         $('#act-title').removeClass('col-5');
         $('#act-desc').addClass('col-12');
-        $('#act-desc').removeClass('col-5');
+        $('#act-desc').removeClass('col-7');
     }
 })
