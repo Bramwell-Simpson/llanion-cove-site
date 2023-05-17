69df2a6ad2e279e07ceb360212d6f2e8dbdb6e66
Bramwell Simpson <bramwellsimpson1@gmail.com>
2023-05-17 13:28:33 +0100

    Fix window.width not being a function

--- src/js/footerFlex.js
+++ src/js/footerFlex.js
@@ -1,10 +1,21 @@
 $(window).on('resize', function() {
-    if((window).Width() < 768) {
-        $('#footer-left').toggleClass("u-justify-flex-end");
-        $('#footer-right').toggleClass("u-justify-flex-start");
+    if($(window).width() > 768) {
+        // $('#footer-left').toggleClass("u-justify-flex-end");
+        // $('#footer-right').toggleClass("u-justify-flex-start");
+
+        //Center contact details without stacking
+        $('#footer-left').addClass("u-justify-flex-end");
+        $('#footer-right').addClass("u-justify-flex-start");
+        $('#footer-left').removeClass("u-justify-center");
+        $('#footer-right').removeClass("u-justify-center");
     }
     else {
-        $('#footer-left').toggleClass('u-justify-flex-end');
-        $('#footer-right').toggleClass("u-justify-flex-start");
+
+        //Remove flex aligns to allow for stacked centering
+        $('#footer-left').removeClass('u-justify-flex-end');
+        $('#footer-right').removeClass("u-justify-flex-start");
+
+        $('#footer-left').addClass("u-justify-center");
+        $('#footer-right').addClass("u-justify-center");
     }
 })
